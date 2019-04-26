f7cbac6e115eaf49ea60303b1b54c8e8f8a9406c
“Francisonly” <swjtufchzh@163.com>
2019-04-26 11:59:30 +0800

    modify Assignment1/MidpointFindingKare

--- Assignment1/MidpointFindingKarel.java
+++ Assignment1/MidpointFindingKarel.java
@@ -1,6 +1,25 @@
 import stanford.karel.*;
 
 public class MidpointFindingKarel extends SuperKarel {
-
-       
+	  public void run() {
+		  conLength();
+		  turnLeft();
+		  turnLeft();
+		  findMid();
+		  putBeeper();
+	  }
+	  private void conLength() {
+		  int i = 1;
+		  while(frontIsClear()) {
+			  move();
+			  i++;
+		  }
+		  j = i+1;
+	  }
+	  private void findMid() {
+		  for(int k = 0; k < (j-1) / 2; k++) {
+			  move();
+		  }
+	  }
+	  private int j;
 }
